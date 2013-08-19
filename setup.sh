a8692b0a8e4672768253471361a1bb8caf89fa10
Lyle Scott, III <lyle@digitalfoo.net>
2013-08-18 20:07:16 -0400

    add setup file to help install dependencies and update the supporting libs.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 # Lyle Scott, III  // lyle@digitalfoo.net // Copyright 2013
 
+if [ ! $(type -P virtualenv) ]; then
+    echo "virtualenv needs to be installed or install the following python "
+    echo "libs manually and skip setup.sh:"
+    cat venv/requirements.txt
+    exit 1
+fi
+
+echo -e "\n#\n# Updating geo dat files...\n#\n"
+./dats/update.sh
+
+echo -e "\n#\n# Updating markerclustererplus js files...\n#\n"
+./html/update.sh
+
+echo -e "\n#\n# Installing virtual environment...\n#\n"
 virtualenv --no-site-packages venv
+
+echo -e "\n#\n# Switching to virtual environment...\n#\n"
 source venv/bin/activate
+
+echo -e "\n#\n# Installing needed python libraries in virtual environment...\n#\n"
 pip install -r venv/requirements.txt
