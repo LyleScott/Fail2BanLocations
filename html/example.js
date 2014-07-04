da1ca38552e130a4f392b4a9e9c989951442b4b1
Lyle <lyle@digitalfoo.net>
2014-07-04 19:19:49 -0400

    update html demo codez

--- html/example.js
+++ html/example.js
@@ -1,22 +1,39 @@
+var infowindow;
+
 function showInfoWindow(cluster) {
+    var markers, center;
+
     if (cluster instanceof Cluster) {
-        var markers = cluster.getMarkers();
-        var center = cluster.getCenter();
+        markers = cluster.getMarkers(),
+        center = cluster.getCenter();
     } else {
-        var markers = [this];
-        var center = this.getPosition();
+        markers = [this];
+        center = this.getPosition();
     }
 
-    var tbl = '<table cellspacing=0 cellpadding=0>';
-    tbl += '<tr><th>&nbsp;</th><th>IP</th><th>Date</th></tr>';
+    var tbl = '<table cellspacing="0" cellpadding="0" width="98%">' +
+              '  <tr>' +
+              '    <th>&nbsp;</th>' +
+              '    <th>IP</th>' +
+              '    <th>City</th>' +
+              '    <th>Country</th>' +
+              '    <th>Service</th>' +
+              '    <th>Source</th>' +
+              '    <th>Dates</th>' +
+              '</tr>';
 
-    for (var i=0; i < markers.length; i++) {
+    for (var i = 0; i < markers.length; i++) {
         var data = markers[i].data;
-        tbl += '<tr>';
-        tbl += '<td>' + (i+1) + '</td>';
-        tbl += '<td>' + data['ip'] + '</td>';
-        tbl += '<td>' + data['date'] + '</td>';
-        tbl += '</tr>';
+
+        tbl += '<tr>' +
+               '  <td>' + (i + 1) + '</td>' +
+               '  <td>' + data['ip'] + '</td>' +
+               '  <td>' + (data['city'] || '&nbsp;') + '</td>' +
+               '  <td>' + data['country_name'] + '</td>' +
+               '  <td>' + data['service'] + '</td>' +
+               '  <td>' + data['source'] + '</td>' +
+               '  <td>' + data['dates'].join('<br>') + '</td>' +
+               '</tr>';
     }
 
     tbl += '</table>';
@@ -34,12 +51,28 @@ function showInfoWindow(cluster) {
 }
 
 
-function initmap() {
-    var center = new google.maps.LatLng(0, 0);
+function build_markers() {
+    var markers = [];
 
+    for (var i = 0; i < locations.length; i++) {
+        var data = locations[i],
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(data.latitude, data.longitude),
+                data: data
+            });
+
+        google.maps.event.addListener(marker, 'click', showInfoWindow);
+        markers.push(marker);
+    }
+
+    return markers;
+}
+
+
+function initmap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 2,
-        center: center,
+        center: new google.maps.LatLng(0, 0),
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         disableDoubleClickZoom: true,
         streetViewControl: false
@@ -50,35 +83,17 @@ function initmap() {
             infowindow.close();
         }
     });
+
     google.maps.event.addListener(map, 'zoom_changed', function() {
         if (typeof infowindow != 'undefined') {
             infowindow.close();
         }
     });
 
-    var markers = [];
-
-    for (var i in locations) {
-        var data = locations[i];
-
-        var latLng = new google.maps.LatLng(
-            data.latitude,
-            data.longitude
-        );
-
-        var marker = new google.maps.Marker({
-            position: latLng,
-            data: data
+    var markers = build_markers(),
+        markerCluster = new MarkerClusterer(map, markers, {
+            zoomOnClick: false
         });
-
-        google.maps.event.addListener(marker, 'click', showInfoWindow);
-
-        markers.push(marker);
-    }
-
-    var markerCluster = new MarkerClusterer(map, markers, {
-        zoomOnClick: false
-    });
 
     google.maps.event.addListener(markerCluster, "click", showInfoWindow);
 }
